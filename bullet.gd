f7dd847de89055707fce5fd2f7af6190a7abb53c
Omaly123 <jonas@linusweb.de>
2023-12-22 14:05:59 +0100

    Bounces YAAAAAAY

--- bullet.gd
+++ bullet.gd
@@ -6,7 +6,7 @@ var SPEED = 600.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2
-var bounces = 5
+var bounces = 0
 var old_velocity : Vector2
 var damage = 10
 var shooter
